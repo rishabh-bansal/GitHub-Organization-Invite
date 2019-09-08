ab3b93fa45b4f16dab200b192fb1fe61f6bd0fa2
Rishabh Bansal <rishabh@Rishabhs-MacBook-Air.local>
2019-09-08 22:33:55 +0530

    first

---
+++ README.md
@@ -0,0 +1,30 @@
+# Automated Github Organization Invites [![Code Climate](https://codeclimate.com/github/thundergolfer/automated-github-organization-invites/badges/gpa.svg)](https://codeclimate.com/github/thundergolfer/automated-github-organization-invites) [![Issue Count](https://codeclimate.com/github/thundergolfer/automated-github-organization-invites/badges/issue_count.svg)](https://codeclimate.com/github/thundergolfer/automated-github-organization-invites)
+
+> Quickly host a webpage allow people to click and receive and invite to your Github Organization
+
+<p align="center">
+  <img src="auto-invites-example.png"/>
+</p>
+
+### Features
+
+* Validates submitted Github usernames
+* Links in your Github Organization's avatar/image
+* Lightweight
+
+### Get It Right Now
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+All you have to do is
+
+1. Click **Deploy to Heroku**
+2. Fill in the **Environment Variables** when prompted. These will be a *[Github Access Token](https://github.com/blog/1509-personal-api-tokens)*, which should have Organisation priviledges enabled, and a *Github Organisation name*, and a *background color*. The choices are `{blue,green,grey,pink,red,white}`. If you don't enter one of those strings it defaults to 'white'.
+
+#### All Done! Just share the Heroku App's URL to people and they'll be able to get themselves an invite to your organisation.
+
+
+#### Credit To *[Code, Applied To Life](https://medium.com/code-applied-to-life/automated-github-organization-invites-3e940aa27040#.sikfvzyaj)* for their efforts which were used as a base for this.
+
+
+
---
+++ web_app.rb
@@ -0,0 +1,92 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'slim'
+require 'octokit'
+
+token = ENV['GITHUB_TOKEN']
+org_name = ENV['ORGANIZATION_NAME']
+background_choice = ENV['BACKGROUND_COLOR']
+
+if background_choice == 'green'
+    background_css = "/css/background_colors/green.css"
+elsif background_choice == 'blue'
+    background_css = "/css/background_colors/blue.css"
+elsif background_choice == 'pink'
+    background_css = "/css/background_colors/pink.css"
+elsif background_choice == 'red'
+    background_css = "/css/background_colors/red.css"
+elsif background_choice == 'grey'
+    background_css = "/css/background_colors/grey.css"
+else
+    background_css = "/css/background_colors/white.css"
+end
+
+html_template_path = File.join(__dir__, 'views', 'index.slim')
+@layout = File.read(html_template_path)
+
+client = Octokit::Client.new(access_token: token)
+
+def user_exists?(client, user)
+  begin
+    profile = client.user(user)
+  rescue Octokit::NotFound
+    return false
+  end
+  return true
+end
+
+def get_org_avatar_url(client, org_name)
+  begin
+    org = client.user(org_name)
+  rescue Octokit::NotFound
+    return nil
+  end
+  org[:avatar_url]
+end
+
+def get_org_id(client, org_name)
+  begin
+    org = client.user(org_name)
+    return org.id
+  rescue Octokit::NotFound
+    return nil
+  end
+end
+
+def get_org_teams(client, org_name)
+  begin
+    teams = client.org_teams(org_name)
+    return teams
+  rescue Octokit::NotFound
+    return nil
+  end
+end
+
+def check_org_exists(client, org_name)
+  unless get_org_avatar_url(client, org_name).nil?
+    return true
+  end
+  return false
+end
+
+# The URL for the Organisation's picture/avatar
+avatar = get_org_avatar_url(client, org_name)
+org_id = get_org_id(client, org_name)
+
+l = Slim::Template.new { @layout }
+
+# ROUTES #
+
+get "/" do
+  slim l.render(Object.new, :avatar => avatar, :org_name => org_name, :background_css => background_css)
+end
+
+post "/add" do
+  if user_exists?(client, params["github"])
+    client.update_organization_membership(org_name, :user => params["github"])
+    "You have been sent an e-mail."
+  else
+    "User not found. Please check your spelling"
+  end
+end
